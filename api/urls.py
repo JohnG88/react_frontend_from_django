27652f0c041a23adb0e2e71a042cc11c243c1068
John Gutierrez <djterez88@gmail.com>
2022-09-20 21:00:49 -0700

    Created simple login with react and django, with react being served in django.

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import login_view, logout_view, session_view, whoami_view
 
 urlpatterns = [
     path('login/', login_view, name='api-login'),
-    path('logout/', login_view, name='api-logout'),
+    path('logout/', logout_view, name='api-logout'),
     path('session/', session_view, name='api-session'),
     path('whoami/', whoami_view, name='api-whoami'),
 ]
--- django_cookie_auth/urls.py
+++ django_cookie_auth/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
+
+def index_view(request):
+    return render(request, 'build/index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', index_view, name='index'),
 ]
